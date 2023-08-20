d547e15a289ea856d24b8203bb69daa9e4f719ee
mcarthuradal <mark.kyama.adal@gmail.com>
2023-08-20 14:02:19 +0300

    defined methods

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,42 +1,124 @@
-﻿
-Random rand = new Random(123);
+// the ourAnimals array will store the following: 
+string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
 
-int healthHero = 100;
-int healthMonster = 100;
-int attack = 0;
-bool drained = false;
+// variables that support data entry
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
 
-Console.WriteLine($"Fight Beginning...\nHero health: {healthHero}");
-Console.WriteLine($"Monster health: {healthMonster}");
+// array used to store runtime data, there is no persisted data
+string[,] ourAnimals = new string[maxPets, 6];
 
-do
+// TODO: Convert the if-elseif-else construct to a switch statement
+// create some initial ourAnimals array entries
+for (int i = 0; i < maxPets; i++)
 {
-//let hero make first attack
-attack = rand.Next(1,11);
-Console.WriteLine("Hero attacking monster...");
-healthMonster -= attack;
-Console.WriteLine($"Health lost: {attack}, health remaining: {healthMonster}");
+    switch (i)
+    {
+        case 0:
+            animalSpecies = "dog";
+            animalID = "d1";
+            animalAge = "2";
+            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+            animalNickname = "lola";
+            break;
 
-// If monster health is available fight back
-if (healthMonster > 0)
-{
-	Console.WriteLine("Monster attacking hero...");
-	attack = rand.Next(1,11);
-	healthHero -= attack;
-	Console.WriteLine($"Health lost: {attack}, health remaining: {healthHero}");
-}
-//If monster dies first
-else if (healthMonster <=0)
-{
-	drained = true;
-	Console.WriteLine("Monster lost, no health remaining");
+        case 1:
+            animalSpecies = "dog";
+            animalID = "d2";
+            animalAge = "9";
+            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+            animalNickname = "loki";
+            break;
 
+        case 2:
+            animalSpecies = "cat";
+            animalID = "c3";
+            animalAge = "1";
+            animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+            animalPersonalityDescription = "friendly";
+            animalNickname = "Puss";
+            break; 
+
+        case 3:
+            animalSpecies = "cat";
+            animalID = "c4";
+            animalAge = "?";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;   
+            
+        default: 
+            animalSpecies = "";
+            animalID = "";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;
+    }
+
+    ourAnimals[i, 0] = "ID #: " + animalID;
+    ourAnimals[i, 1] = "Species: " + animalSpecies;
+    ourAnimals[i, 2] = "Age: " + animalAge;
+    ourAnimals[i, 3] = "Nickname: " + animalNickname;
+    ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+    ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
 }
-// If hero dies first
-if (healthHero <=0) 
-{
-	drained = true;
-	Console.WriteLine("Hero lost, no health remaining");
+
+// display the top-level menu options
+do {
+
+    Console.Clear();
+
+    Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+    Console.WriteLine(" 1. List all of our current pet information");
+    Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+    Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+    Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+    Console.WriteLine(" 5. Edit an animal’s age");
+    Console.WriteLine(" 6. Edit an animal’s personality description");
+    Console.WriteLine(" 7. Display all cats with a specified characteristic");
+    Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+    Console.WriteLine();
+    Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+    readResult = Console.ReadLine();
+
+    if (readResult != null)
+    {
+        menuSelection = readResult.ToLower();
+    }
+
+    Console.WriteLine($"You selected menu option {menuSelection}.");
+    Console.WriteLine("Press the Enter key to continue");
+
+    // pause code execution
+    readResult = Console.ReadLine();
+
+    switch (menuSelection)
+    {
+        case "1":
+            // List all of our current pet information
+            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        case "2":
+            // List all of our current pet information
+            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+    }
 }
-}
-while (drained == false);
+while (menuSelection != "exit");
---
+++ done/loops.cs
@@ -0,0 +1,42 @@
+﻿
+Random rand = new Random(123);
+
+int healthHero = 100;
+int healthMonster = 100;
+int attack = 0;
+bool drained = false;
+
+Console.WriteLine($"Fight Beginning...\nHero health: {healthHero}");
+Console.WriteLine($"Monster health: {healthMonster}");
+
+do
+{
+//let hero make first attack
+attack = rand.Next(1,11);
+Console.WriteLine("Hero attacking monster...");
+healthMonster -= attack;
+Console.WriteLine($"Health lost: {attack}, health remaining: {healthMonster}");
+
+// If monster health is available fight back
+if (healthMonster > 0)
+{
+	Console.WriteLine("Monster attacking hero...");
+	attack = rand.Next(1,11);
+	healthHero -= attack;
+	Console.WriteLine($"Health lost: {attack}, health remaining: {healthHero}");
+}
+//If monster dies first
+else if (healthMonster <=0)
+{
+	drained = true;
+	Console.WriteLine("Monster lost, no health remaining");
+
+}
+// If hero dies first
+if (healthHero <=0) 
+{
+	drained = true;
+	Console.WriteLine("Hero lost, no health remaining");
+}
+}
+while (drained == false);
